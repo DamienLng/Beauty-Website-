6ad900e803fd93e02ec036fb5923b62f38770c48
Damien Leung <17614@burnside.school.nz>
2021-07-11 15:07:43 +1200

    Made joining table for Products and Category, Fixed broken css navigation bar, Tried to sucessfully create query but still doesn't work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from os import name
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, model
 from config import Config
 
 app = Flask(__name__)
@@ -12,26 +13,42 @@ import models
 def home():
     return render_template('home.html', page_title="HOME")
 
-@app.route('/categories/<int:id>')
-def category(id):
-    print(id)
-    category = models.Category.query.all()
-    return render_template('category.html', category = category)
+# @app.route('/categories/<int:id>')
+# def category(id):
+#     print(id)
+#     category = models.Category.query.all()
+#     return render_template('category.html', category = category)
 
-@app.route('/subcategory/<int:id>')
+
+'''TODO: Make relationship table between Product and Category Table to see 
+if this fixes the .products relationship problem'''
+# This is Mr Dunford's Query/Explanation, and this is also connected to the variable name of products in models.py
+@app.route("/skincare/subcategory/all_skincare/<string:name>")
+def all_skincare(name):
+    cat = models.Category.query.filter_by(name=name).first_or_404()
+    print(cat)
+    print(cat.products) 
+    return render_template('all_skincare.html', cat = cat)
+
+
+
+@app.route('/skincare/<int:id>')
 def subcategory(id):
     subcategory = models.Subcategory.query.all()
     return render_template('subcategory.html', subcategory = subcategory)
-
-@app.route('/product/<int:id>')
-def product(id):
-    product = models.Product.query.all()
-    return render_template('product.html', product = product)
 
 @app.route('/wishlist/<int:id>')
 def wishlist(id):
     return render_template('wishlist.html', wishlist = wishlist)
 
+# @app.route('/product/<int:id>')
+# def product(id):
+#     product = models.Product.query.all()
+#     return render_template('product.html', product = product)
+
+@app.route('/best_sellers/<int:id>')
+def best_sellers(id):
+    return render_template('best_sellers.html', best_sellers = best_sellers)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,16 +1,20 @@
+from sqlalchemy.orm import backref
 from main import db
 
-Productsubcategory = db.Table('Productsubcategory', db.Model.metadata,
-                    db.Column('Product_id', db.ForeignKey('Product.id')),
-                    db.Column('Subcategory_id', db.ForeignKey('Subcategory.id'))
-                   )
+Productcategory = db.Table('Productcategory', db.Model.metadata,
+                    db.Column('Product_id', db.Integer, db.ForeignKey('Product.id')),
+                    db.Column('Category_id', db.Integer, db.ForeignKey('Category.id'))
+                    )
 
+Productsubcategory = db.Table('Productsubcategory', db.Model.metadata,
+                    db.Column('Product_id', db.Integer, db.ForeignKey('Product.id')),
+                    db.Column('Subcategory_id', db.Integer, db.ForeignKey('Subcategory.id'))
+                   )
 
 Brandcategory = db.Table('Brandcategory', db.Model.metadata,
                     db.Column('Brand_id', db.ForeignKey('Brand.id')),
                     db.Column('Category_id', db.ForeignKey('Category.id'))
                    )
-
 
 Brandsubcategory = db.Table('Brandsubcategory', db.Model.metadata,
                     db.Column('Brand_id', db.ForeignKey('Brand.id')),
@@ -19,17 +23,22 @@ Brandsubcategory = db.Table('Brandsubcategory', db.Model.metadata,
 
 
 class Category(db.Model):
+
   __tablename__ = 'Category'
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String())
   description = db.Column(db.Text())
-  brands = db.relationship('Brand', secondary=Brandcategory, back_populates='category')
+
+  product = db.relationship('Product', secondary=Productcategory, back_populates='productcategory')
+  brands = db.relationship('Brand', secondary=Brandcategory, back_populates='brandcategory')
+  products = db.relationship('Product', backref="categories")
 
 
 class Brand(db.Model):
   __tablename__ = 'Brand'
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String())
+  
   subcategory = db.relationship('Subcategory', secondary=Brandsubcategory, back_populates='brands')
   category = db.relationship('Category', secondary=Brandcategory, back_populates='brands')
 
@@ -39,20 +48,25 @@ class Subcategory(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String())
   description = db.Column(db.Text())
+
   brands = db.relationship('Brand', secondary=Brandsubcategory, back_populates='subcategory')
   products = db.relationship('Product', secondary=Productsubcategory, back_populates='subcategory')
 
 
 class Product(db.Model):
   __tablename__ = 'Product'
+
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String())
   description = db.Column(db.Text())
   price = db.Column(db.String())
   image = db.Column(db.String())
   ingredients = db.Column(db.Text())
-  category = db.Column(db.Integer, foreign_key=True)
-  brand = db.Column(db.Integer, foreign_key=True)
-  subcategory = db.relationship('Subcategory', secondary=Productsubcategory, back_populates='products')
+  category = db.Column(db.Integer, db.ForeignKey("Category.id"))
+  brand = db.Column(db.Integer, db.ForeignKey("Brand.id"))
 
+  category = db.relationship('Category', secondary=Productcategory, back_populates='products')
+  subcategory = db.relationship('Subcategory', secondary=Productsubcategory, back_populates='products')
   
+
+
