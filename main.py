7902f9da433402a0465649b713dda1524e2085d9
Damien Leung <17614@burnside.school.nz>
2021-10-26 11:28:50 +1300

    Fixed/cleaned up some coding eg, ensure only one body and main tag in each html document

--- main.py
+++ main.py
@@ -25,12 +25,26 @@ def subcat(category, name):
     return render_template('subcategory.html', subcat = subcat, products = Subcategory.products)
 
 # Subcategory page for different brands of products
-@app.route('/<string:category>/<string:name>')
+@app.route('/<string:table>/<string:name>')
 def brands(category, name):
     brands = models.Product.query.filter_by(name=name).all()
     for Product in brands:
         print(Product.products)
     return render_template('brand.html', brands = brands, products = Product.products)
+
+# if table is a brand.name 
+# then models.Brand.query.filter_by(name=name).all()
+
+# if table = subcategory 
+# then models.Subcategory.query.filter_by(name=name).all()
+
+# this is all under one route @app.route('/<string:table>/<string:name>') with one function name (name has to 
+# make sense with both things im trying to query eg, brands and other subcatories), then 
+# write render template thing with both html links in there etc think logically to piece the rest together. 
+# Furthermore, I may need to fix up models.py to fully make brands query functional. In the end
+# I will only have one route querying two different things. So i can delete the subcat query. 
+
+
 
 # Individual Product Pages 
 @app.route('/product/<int:id>')
