331c67806d4c1f83bff084136282d582603ac251
Damien Leung <17614@burnside.school.nz>
2021-10-29 13:13:49 +1300

    Deleted wishlist, category and brand html, deleted some tables from database, created 404 error page with CSS

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from os import name, replace
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, abort
 from flask_sqlalchemy import SQLAlchemy, model
 from config import Config
 
@@ -9,6 +9,13 @@ db = SQLAlchemy(app)
 
 import models 
 
+
+# Reroute when a 404 error is found
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
 # Homepage route/query for all Products
 @app.route('/')
 def home():
@@ -16,34 +23,17 @@ def home():
     # print(products)
     return render_template('home.html', products = products)
 
+
 # Subcategory pages for skincare, makeup and hair, hand & body
 @app.route('/<string:category>/<string:name>')
 def subcat(category, name):
-    subcat = models.Subcategory.query.filter_by(name=name).all()
-    for Subcategory in subcat:
-        print(Subcategory.products)
-    return render_template('subcategory.html', subcat = subcat, products = Subcategory.products)
-
-# Subcategory page for different brands of products
-@app.route('/<string:table>/<string:name>')
-def brands(category, name):
-    brands = models.Product.query.filter_by(name=name).all()
-    for Product in brands:
-        print(Product.products)
-    return render_template('brand.html', brands = brands, products = Product.products)
-
-# if table is a brand.name 
-# then models.Brand.query.filter_by(name=name).all()
-
-# if table = subcategory 
-# then models.Subcategory.query.filter_by(name=name).all()
-
-# this is all under one route @app.route('/<string:table>/<string:name>') with one function name (name has to 
-# make sense with both things im trying to query eg, brands and other subcatories), then 
-# write render template thing with both html links in there etc think logically to piece the rest together. 
-# Furthermore, I may need to fix up models.py to fully make brands query functional. In the end
-# I will only have one route querying two different things. So i can delete the subcat query. 
-
+    try:
+        subcat = models.Subcategory.query.filter_by(name=name).all()
+        for Subcategory in subcat:
+            print(Subcategory.products)
+        return render_template('subcategory.html', subcat = subcat, products = Subcategory.products)
+    except:
+        abort(404)
 
 
 # Individual Product Pages 
@@ -51,6 +41,7 @@ def brands(category, name):
 def product(id): 
     product = models.Product.query.filter_by(id=id).first_or_404()
     return render_template('product.html', product = product)
+
 
 # Contact Us Page 
 @app.route('/contact_us')
--- models.py
+++ models.py
@@ -1,25 +1,25 @@
 from sqlalchemy.orm import backref
 from main import db
 
-ProductCategory = db.Table('ProductCategory', db.Model.metadata,
-                    db.Column('pid', db.Integer, db.ForeignKey('Product.id')),
-                    db.Column('cid', db.Integer, db.ForeignKey('Category.id'))
-                    )
+# ProductCategory = db.Table('ProductCategory', db.Model.metadata,
+#                     db.Column('pid', db.Integer, db.ForeignKey('Product.id')),
+#                     db.Column('cid', db.Integer, db.ForeignKey('Category.id'))
+#                     )
 
 ProductSubcategory = db.Table('ProductSubcategory', db.Model.metadata,
                     db.Column('pid', db.Integer, db.ForeignKey('Product.id')),
                     db.Column('sid', db.Integer, db.ForeignKey('Subcategory.id'))
                    )
 
-BrandCategory = db.Table('BrandCategory', db.Model.metadata,
-                    db.Column('bid', db.ForeignKey('Brand.id')),
-                    db.Column('cid', db.ForeignKey('Category.id'))
-                   )
+# BrandCategory = db.Table('BrandCategory', db.Model.metadata,
+#                     db.Column('bid', db.ForeignKey('Brand.id')),
+#                     db.Column('cid', db.ForeignKey('Category.id'))
+#                    )
 
-BrandSubcategory = db.Table('BrandSubcategory', db.Model.metadata,
-                    db.Column('bid', db.ForeignKey('Brand.id')),
-                    db.Column('sid', db.ForeignKey('Subcategory.id'))
-                   )
+# BrandSubcategory = db.Table('BrandSubcategory', db.Model.metadata,
+#                     db.Column('bid', db.ForeignKey('Brand.id')),
+#                     db.Column('sid', db.ForeignKey('Subcategory.id'))
+#                    )
 
 class Category(db.Model):
   __tablename__ = 'Category'
@@ -28,14 +28,13 @@ class Category(db.Model):
   name = db.Column(db.String())
   description = db.Column(db.Text())
 
-  products = db.relationship('Product', secondary=ProductCategory, back_populates='category')
-  # brands = db.relationship('Brand', secondary=Brandcategory, back_populates='brandcategory')
+  # products = db.relationship('Product', secondary=ProductCategory, back_populates='category')
 
 
-class Brand(db.Model):
-  __tablename__ = 'Brand'
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String())
+# class Brand(db.Model):
+#   __tablename__ = 'Brand'
+#   id = db.Column(db.Integer, primary_key=True)
+#   name = db.Column(db.String())
   
 #   subcategory = db.relationship('Subcategory', secondary=BrandSubcategory, back_populates='brands')
 #   category = db.relationship('Category', secondary=BrandCategory, back_populates='brands')
@@ -63,9 +62,8 @@ class Product(db.Model):
   price = db.Column(db.String())
   image = db.Column(db.String())
   ingredients = db.Column(db.Text())
-  brand = db.Column(db.Integer, db.ForeignKey("Brand.id"))
 
-  category = db.relationship('Category', secondary=ProductCategory, back_populates='products')
+  # category = db.relationship('Category', secondary=ProductCategory, back_populates='products')
   subcategory = db.relationship('Subcategory', secondary=ProductSubcategory, back_populates='products')
 
 
